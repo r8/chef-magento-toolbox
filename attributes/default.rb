f9f51e57882e8e15e13260ba0f7fba1c732fc16e
Sergey Storchay <r8@r8.com.ua>
2013-08-04 15:22:27 +0300

    Add docblocks

---
+++ attributes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: magento-toolbox
+# Attributes:: default
+#
+# Copyright 2013, Sergey Storchay
+#
+# Licensed under MIT:
+# http://raw.github.com/r8/magento-toolbox/master/LICENSE.txt
+
---
+++ recipes/n98-magerun.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: magento-toolbox
+# Recipe:: n98-magerun
+#
+# Copyright 2013, Sergey Storchay
+#
+# Licensed under MIT:
+# http://raw.github.com/r8/magento-toolbox/master/LICENSE.txt
+
