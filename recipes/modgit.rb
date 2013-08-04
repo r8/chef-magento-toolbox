b6dc44d2a22487d409ca172e7d7190bbd06c9bed
Sergey Storchay <r8@r8.com.ua>
2013-08-04 15:27:10 +0300

    Add placeholder recipes

---
+++ recipes/modgit.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: magento-toolbox
+# Recipe:: modgit
+#
+# Copyright 2013, Sergey Storchay
+#
+# Licensed under MIT:
+# http://raw.github.com/r8/magento-toolbox/master/LICENSE.txt
+
---
+++ recipes/modman.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: magento-toolbox
+# Recipe:: modman
+#
+# Copyright 2013, Sergey Storchay
+#
+# Licensed under MIT:
+# http://raw.github.com/r8/magento-toolbox/master/LICENSE.txt
+
