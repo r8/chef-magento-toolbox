eb66cf91d22fc111bf2516fa73296e2adbb7c08d
Sergey Storchay <r8@r8.com.ua>
2013-08-04 16:23:52 +0300

    Implement modman and modgit recipes

--- recipes/modman.rb
+++ recipes/modman.rb
@@ -7,3 +7,24 @@
 # Licensed under MIT:
 # http://raw.github.com/r8/magento-toolbox/master/LICENSE.txt
 
+package "curl" do
+  action :upgrade
+end
+
+command = "curl -o modman https://raw.github.com/colinmollenhour/modman/master/modman && chmod +x ./modman"
+
+bash "download_modman" do
+  cwd "#{Chef::Config[:file_cache_path]}"
+  code <<-EOH
+    #{command}
+  EOH
+end
+
+if node["modman"]["install_globally"]
+  bash "move_modman" do
+    cwd "#{Chef::Config[:file_cache_path]}"
+    code <<-EOH
+      sudo mv modman #{node["modman"]["prefix"]}/bin/modman
+    EOH
+  end
+end
